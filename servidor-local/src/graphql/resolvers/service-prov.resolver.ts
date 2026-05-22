e582380c4a3a7e0e753a70dc7c20f5a6df74c2bc
TiagoSoares2334 <elvizoarez1@gmail.com>
2026-05-22 10:22:04 -0100

    refactor: migrate legacy project files to legacy directory and perform code cleanup

--- servidor-local/src/graphql/resolvers/service-prov.resolver.ts
+++ servidor-local/src/graphql/resolvers/service-prov.resolver.ts
@@ -1,45 +1,41 @@
 import { budgetModel } from "../../models/orcamentos.model.js";
-import { serviceProvModel } from "../../models/prestacao_servico.models.js";
-import { ProviderModel } from "../../models/prestador.model.js";
-import { proposalModel } from "../../models/proposta.models.js";
+import { PrestacaoServicoModel } from "../../models/prestacao-servico.model.js";
 import { ServiceModel } from "../../models/servico.model.js";
-import type { ServiceProvType } from "../../utils/types.js";
-
-
+import { PrestadorDBType } from "../../utils/types.js";
 
 export const ServiceProvResolver = {
     Query: {
         getAllServiceProv: async () => {
-            return await serviceProvModel.getAll();
+            return await PrestacaoServicoModel.getAll();
         },
-        getServiceProvById: async (_: any, args: {id: string}) => {
-            return await serviceProvModel.get(args.id);
+        getServiceProvById: async (_: any, args: { id: string }) => {
+            return await PrestacaoServicoModel.get(args.id);
         }
     },
     Mutation: {
-        createServiceProv: async (_: any, args: {serviceProv: ServiceProvType}) => {
-            return await serviceProvModel.create(args.serviceProv);
+        createServiceProv: async (_: any, args: { serviceProv: PrestadorDBType }) => {
+            return await PrestacaoServicoModel.create(args.serviceProv);
         },
-        updateServiceProv: async (_: any, args: {id: string, newServiceProv: ServiceProvType}) => {
-            return await serviceProvModel.update(args.id, args.newServiceProv);
+        updateServiceProv: async (_: any, args: { id: string, newServiceProv: PrestadorDBType }) => {
+            return await PrestacaoServicoModel.update(args.id, args.newServiceProv);
         },
-        deleteServiceProv: async (_: any, args: {id: string}) => {
-            return await serviceProvModel.delete(args.id);
+        deleteServiceProv: async (_: any, args: { id: string }) => {
+            return await PrestacaoServicoModel.delete(args.id);
         }
     },
     // Relacionamentos de tabelas
     serviceProv: {
-        servico: async (parent: {id: string}) => {
+        servico: async (parent: { id: string }) => {
             return await ServiceModel.get(parent.id);
         },
-        budget: async (parent: {id: string}) => {
+        budget: async (parent: { id: string }) => {
             return await budgetModel.get(parent.id);
         },
-        prestador: async (parent: {id: string}) => {
-            return await ProviderModel.get(parent.id);
+        prestador: async (parent: { id: string }) => {
+            return await PrestadorModel.get(parent.id);
         },
-        proposta: async (parent: {id: string}) => {
-            return await proposalModel.get(parent.id);
+        proposta: async (parent: { id: string }) => {
+            return await PropostaModel.get(parent.id);
         }
     }
 }
