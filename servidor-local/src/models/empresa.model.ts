7f8b6fe39010ca0b247d1c73cef0feb06cf13e99
Djeison Santos <djeison.adventista@gmail.com>
2026-05-26 11:15:09 -0100

    refactor: update database queries in EmpresaModel and OrcamentoModel to use PostgreSQL and improve error handling

--- servidor-local/src/models/empresa.model.ts
+++ servidor-local/src/models/empresa.model.ts
@@ -1,16 +1,15 @@
 
-import type { RowDataPacket } from "mysql2";
-import db from "../lib/db.js";
+import db from "../lib/db-pg.js";
 import type { EmpresaDBType } from "../utils/types.js";
 import { generateUUID } from "../utils/uuid.js";
 
 export const EmpresaModel = {
     async create(empresa: EmpresaDBType): Promise<EmpresaDBType | null> {
         try {
-            const result = await db.query<EmpresaDBType & RowDataPacket[]>(
+            const result = await db.query<EmpresaDBType>(
                 `INSERT INTO tbl_empresa
                 (id, designacao, descricao, localizacao, nif, icone, id_utilizador, enabled, created_at, updated_at)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) Returning *`,
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`,
                 [
                     generateUUID(),
                     empresa.designacao,
@@ -24,46 +23,45 @@ export const EmpresaModel = {
                     new Date(),
                 ]
             );
-            return result[0] as EmpresaDBType;
+            return result.rows[0] || null;
         } catch (error) {
-            console.log(error);
+            console.error(error);
             return null;
         }
     },
 
-    async getAll(): Promise<EmpresaDBType[] | null> {
+    async getAll(): Promise<EmpresaDBType[]> {
         try {
-            const result = await db.query<EmpresaDBType[] & RowDataPacket[]>(
+            const result = await db.query<EmpresaDBType>(
                 `SELECT * FROM tbl_empresa`
             );
-            return result[0] as EmpresaDBType[];
+            return result.rows;
         } catch (error) {
-            console.log(error);
-            return null;
+            console.error(error);
+            return [];
         }
     },
 
     async get(id: string): Promise<EmpresaDBType | null> {
         try {
-            const result = await db.query<EmpresaDBType[] & RowDataPacket[]>(
+            const result = await db.query<EmpresaDBType>(
                 `SELECT * FROM tbl_empresa WHERE id = $1`,
                 [id]
             );
 
-            if (Array.isArray(result[0]) && result[0].length === 0) return null;
-            return Array.isArray(result[0]) ? result[0][0] as EmpresaDBType : null;
+            return result.rows[0] || null;
         } catch (error) {
-            console.log(error);
+            console.error(error);
             return null;
         }
     },
 
     async update(id: string, empresa: EmpresaDBType): Promise<EmpresaDBType | null> {
         try {
-            const result = await db.query<EmpresaDBType & RowDataPacket[]>(
+            const result = await db.query<EmpresaDBType>(
                 `UPDATE tbl_empresa
                 SET designacao = $1, descricao = $2, localizacao = $3, nif = $4, icone = $5, id_utilizador = $6, enabled = $7, updated_at = $8
-                WHERE id = $9 Returning *`,
+                WHERE id = $9 RETURNING *`,
                 [
                     empresa.designacao,
                     empresa.descricao,
@@ -76,23 +74,23 @@ export const EmpresaModel = {
                     id,
                 ]
             );
-            return result[0] || null;
+            return result.rows[0] || null;
         } catch (error) {
-            console.log(error);
+            console.error(error);
             return null;
         }
     },
 
     async delete(id: string): Promise<EmpresaDBType | null> {
         try {
-            const [result] = await db.query(
-                `DELETE FROM tbl_empresa WHERE id = $1 Returning *`,
+            const result = await db.query<EmpresaDBType>(
+                `DELETE FROM tbl_empresa WHERE id = $1 RETURNING *`,
                 [id]
             );
 
-            return result.rowCount === 0 ? null : result.rows[0];
+            return result.rows[0] || null;
         } catch (error) {
-            console.log(error);
+            console.error(error);
             return null;
         }
     },
