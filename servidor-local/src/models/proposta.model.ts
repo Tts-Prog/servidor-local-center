6cde3738a18f2c04db389d201ef010f9fc45d376
rafa <monteir20@icloud.com>
2026-05-26 11:43:58 -0100

    updated

--- servidor-local/src/models/proposta.model.ts
+++ servidor-local/src/models/proposta.model.ts
@@ -1,6 +1,6 @@
 
 // import type { RowDataPacket } from "mysql2"
-import db from "../lib/db.js"
+import db from "../lib/db-pg.js"
 import type { PropostaDBType } from "../utils/types.js"
 import { generateUUID } from "../utils/uuid.js"
 
@@ -8,7 +8,7 @@ import { generateUUID } from "../utils/uuid.js"
 export const PropostaModel = {
     async create(proposta: PropostaDBType): Promise<PropostaDBType | null> {
         try {
-            const result = await db.query<PropostaDBType[]>(
+            const result = await db.query<PropostaDBType>(
                 `INSERT INTO tbl_proposta 
                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
 
@@ -31,21 +31,21 @@ export const PropostaModel = {
     },
 
     async getAll(): Promise<PropostaDBType[] | null> {
-        const result = await db.query("SELECT * FROM tbl_proposta RETURNING *")
+        const result = await db.query("SELECT * FROM tbl_proposta")
 
         return result.rows as PropostaDBType[]
     },
 
     async get(id: string): Promise<PropostaDBType | null> {
         try {
-            const [rows] = await db.query<PropostaDBType[]>(
+            const rows = await db.query<PropostaDBType[]>(
                 `SELECT DISTINCT 
                 pt.*,
                 pr.id as ownwer
                 FROM tbl_proposta pt
                 INNER JOIN tbl_prestadores pr ON pt.id_prestador = pr.id
                 INNER JOIN tbl_utilizadores u ON pr.id_utilizador = u.id
-                WHERE pt.id = $1 RETURNING *`,
+                WHERE pt.id = $1`,
 
                 [id]
             )
@@ -60,7 +60,7 @@ export const PropostaModel = {
 
     async update(id: string, proposta: PropostaDBType) {
         try {
-            const result = await db.query<PropostaDBType[]>(
+            const result = await db.query<PropostaDBType>(
                 `UPDATE tbl_proposta
                 SET id_prestacao_servico = $1, 
                 preco_hora = $2, 
@@ -94,13 +94,13 @@ export const PropostaModel = {
 
             // 1. marcar proposta como aceite
             await db.query<PropostaDBType[]>(
-                `UPDATE tbl_proposta SET estado = 'Aceite' WHERE id = $1 RETURNING *`,
+                `UPDATE tbl_proposta SET estado = 'Aceite' WHERE id = $1`,
                 [id]
             );
 
             // 2. buscar proposta
             const result = await db.query<PropostaDBType[]>(
-                `SELECT * FROM tbl_proposta WHERE id = $1 RETURNING *`,
+                `SELECT * FROM tbl_proposta WHERE id = $1`,
                 [id]
             );
 
@@ -110,16 +110,16 @@ export const PropostaModel = {
             await db.query<PropostaDBType[]>(
                 `UPDATE tbl_prestacao_servico 
              SET estado = 'Aceite'
-             WHERE id = $1 RETURNING *`,
-                [proposta.id_prestacao_servico]
+             WHERE id = $1`,
+                [id]
             );
 
             // 4. rejeitar restantes propostas
             await db.query<PropostaDBType[]>(
                 `UPDATE tbl_proposta 
              SET estado = 'Rejeitada'
-             WHERE id_prestacao_servico = $1 AND id != $2 RETURNING *`,
-                [proposta.id_prestacao_servico, id]
+             WHERE id_prestacao_servico = $1 AND id != $2`,
+                [id]
             );
 
             return null;
@@ -133,14 +133,14 @@ export const PropostaModel = {
 
     async delete(id: string): Promise<PropostaDBType | null> {
         try {
-            const result = await db.query<PropostaDBType[]>(
+            const result = await db.query<PropostaDBType>(
                 `DELETE FROM tbl_proposta 
-                WHERE id = $1 RETURNING *`,
+                WHERE id = $1`,
 
                 [id]
             )
 
-            return result.rows[0]
+            return result.rows[0] as PropostaDBType
         } catch (err) {
             console.log(err)
             return null
@@ -151,25 +151,25 @@ export const PropostaModel = {
 
     async getByPrestacaoServico(idPrestacaoServico: string): Promise<PropostaDBType[] | null> {
         try {
-            const result = await db.query<PropostaDBType[]>(
+            const result = await db.query<PropostaDBType>(
                 `SELECT * FROM tbl_proposta 
-                    WHERE tbl_proposta.id_prestacao_servico = $1 RETURNING *`,
+                    WHERE tbl_proposta.id_prestacao_servico = $1`,
                 [idPrestacaoServico]
             )
             if (result.rows.length === 0) return null
             return Array.isArray(result.rows) ? result.rows as PropostaDBType[] : null
         } catch (err) {
             console.log(err)
-            return null 
+            return null
         }
     },
 
     async acceptProposal(id: string): Promise<PropostaDBType | null> {
         try {
-            const result = await db.query<PropostaDBType[]>(
+            const result = await db.query<PropostaDBType>(
                 `UPDATE tbl_proposta 
                 SET estado = 'ACEITE' 
-                WHERE id = $1 RETURNING *`,
+                WHERE id = $1`,
                 [id])
             return result.rows[0] as PropostaDBType
         } catch (err) {
