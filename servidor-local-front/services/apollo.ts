f69d966931628d57078b611f2fbc64050f728583
Fábio Barros <brainsonfire85@gmail.com>
2026-06-05 10:19:08 -0100

    atualizando código do gulugulu

--- servidor-local-front/services/apollo.ts
+++ servidor-local-front/services/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const graphqlUri =
-  process.env.NEXT_PUBLIC_DATA_API_URL ?? "http://localhost:8080/graphql";
+  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080/graphql";
 
 const client = new ApolloClient({
   link: new HttpLink({
@@ -9,5 +9,7 @@ const client = new ApolloClient({
   }),
   cache: new InMemoryCache(),
 });
+
+
 
 export default client;
