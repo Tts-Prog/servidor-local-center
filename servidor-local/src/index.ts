02c9ebd4a9bdc10acc35ba93be7481ab64b53114
TiagoSoares2334 <elvizoarez1@gmail.com>
2026-06-09 11:15:24 -0100

    feat: allow server port configuration via environment variable

--- servidor-local/src/index.ts
+++ servidor-local/src/index.ts
@@ -74,6 +74,8 @@ const sslOptions = {
     cert: fs.readFileSync('./cert/server.cert')
 };
 
-https.createServer(sslOptions, app).listen(8080, () => {
-    console.log("Servidor rodando em https://localhost:8080");
+const PORT = process.env.PORT ?? 8080;
+
+https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`Servidor rodando em https://localhost:${PORT}`);
 });
