f34108047581cba3e54ef373e6b400b540c674fa
Kristian Alves <kristianalves307@gmail.com>
2026-06-09 10:22:39 -0100

    change

--- servidor-local/src/lib/db-pg.ts
+++ servidor-local/src/lib/db-pg.ts
@@ -1,13 +1,14 @@
 import { Pool } from "pg";
 
 const db = new Pool({
-    host: process.env.DB_PG_HOST || "localhost",
-    user: process.env.DB_PG_USER || "postgres",
-    password: process.env.DB_PG_PASSWORD || "sua_senha",
-    database: process.env.DB_PG_NAME || "servidor_local",
-    port: Number(process.env.DB_PG_PORT) || 5432,
-    ssl: process.env.DB_PG_SSL === "true" ? { rejectUnauthorized: false } : undefined
-
+    host: process.env.Hostname || "localhost",
+    user: process.env.Username || "postgres",
+    password: process.env.Password || "sua_senha",
+    database: process.env.Database || "servidor_local",
+    port: Number(process.env.Port) || 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 db.connect()
     .then(() => console.log("Conexão com o banco de dados PostgreSQL estabelecida com sucesso!"))
