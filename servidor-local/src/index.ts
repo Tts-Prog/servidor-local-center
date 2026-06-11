8af44ab972813e04058f5d832613d0cd5d07660b
Kristian Alves <kristianalves307@gmail.com>
2026-06-11 09:15:53 -0100

    changes

--- servidor-local/src/index.ts
+++ servidor-local/src/index.ts
@@ -69,13 +69,21 @@ app.use("/graphql", expressMiddleware(graphqlServer, {
 }))
 
 // inicia o servidor na porta 8080 com SSL
-const sslOptions = {
-    key: fs.readFileSync('./cert/server.key'),
-    cert: fs.readFileSync('./cert/server.cert')
+if (process.env.NODE_ENV === "development") {
+    // inicia o servidor na porta 8080 com SSL
+    const sslOptions = {
+        key: fs.readFileSync('./cert/server.key'),
+        cert: fs.readFileSync('./cert/server.cert')
+    };
+
+    const PORT = process.env.PORT ?? 8080;
+
+    https.createServer(sslOptions, app).listen(PORT, () => {
+        console.log(`Servidor rodando em https://localhost:${PORT}`);
+    });
+} else {
+    const PORT = process.env.PORT ?? 8080;
+    app.listen(parseInt(PORT as string, 10), "0.0.0.0", () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
 };
-
-const DB_PORT = process.env.PORT ?? 8080;
-
-https.createServer(sslOptions, app).listen(DB_PORT, () => {
-    console.log("Servidor rodando");
-});
