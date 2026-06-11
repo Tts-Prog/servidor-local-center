e3650460f364baa525cee3c90db6d93d8014f56c
Kristian Alves <kristianalves307@gmail.com>
2026-06-11 09:15:53 -0100

    changes

--- servidor-local/src/index.ts
+++ servidor-local/src/index.ts
@@ -116,7 +116,6 @@ app.use("/graphql", expressMiddleware(graphqlServer, {
 }))
 
 // inicia o servidor na porta 8080 com SSL
-
 if (process.env.NODE_ENV === "development") {
     // inicia o servidor na porta 8080 com SSL
     const sslOptions = {
@@ -135,4 +134,3 @@ if (process.env.NODE_ENV === "development") {
         console.log(`Servidor rodando em http://localhost:${PORT}`);
     });
 };
-
