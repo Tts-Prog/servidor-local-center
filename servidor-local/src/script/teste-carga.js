ab8c8117801bc7def04909f0398975d7d22eda4e
jaquilinagomes <jakscv@gmail.com>
2026-06-22 11:16:50 -0100

    add

--- servidor-local/src/script/teste-carga.js
+++ servidor-local/src/script/teste-carga.js
@@ -7,24 +7,24 @@ const options = {
 };
 
 export default function () {
-  const url =
+    const url =
     "https://servidor-local-center-backend-wt4z.onrender.com/users/login"; // URL do endpoint a ser testado
 
-  const payload = JSON.stringify({
+    const payload = JSON.stringify({
     email: "z@gmail.com",
     password: "9999",
-  });
+    });
 
-  const headers = {
+    const headers = {
     "Content-Type": "application/json",
-  };
+    };
 
-  const response = http.post(url, payload, { headers });
-  check(response, {
+    const response = http.post(url, payload, { headers });
+    check(response, {
     "Login Bem-sucedido": (r) => r.status === 200,
     "Login Rapido (Tempo < 500ms)": (r) => r.timings.duration < 500, // tempo de resposta menor que 500ms
     "CPU Esgotado (Erro 502/504)": (r) => r.status >= 500, // não deve retornar erro de CPU esgotado
-  });
+    });
 
   sleep(1); // espera 1 segundo entre as requisições
 }
