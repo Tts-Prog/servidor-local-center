1e280cab0b6b4e754c033ef15f82829d054d1a4d
TiagoSoares2334 <elvizoarez1@gmail.com>
2026-06-23 09:22:04 -0100

    feat: update CORS whitelist and fix load test URL protocol

--- servidor-local/src/index.ts
+++ servidor-local/src/index.ts
@@ -26,7 +26,29 @@ app.use(express.json()); // para interpretar o corpo das requisições como JSON
 
 // liberta o front-end de aceder ao back-end
 app.use(cors({
-    origin: ["http://localhost:3000", "https://servidor-local-center-backend2.onrender.com", "https://servidor-local-front-ts.vercel.app", "https://servidor-local-center-three.vercel.app"],
+    origin: [
+        "http://localhost:3000",
+        "https://stiven-gulugulu.vercel.app",
+        "https://bruno-gulugulu.vercel.app",
+        "https://gulugulu-gray.vercel.app",
+        "https://servidor-local-front-me74.vercel.app",
+        "https://servidor-local-center-backend2.onrender.com",
+        "https://servidor-local-front-ts.vercel.app",
+        "https://servidor-local-center-three.vercel.app",
+        "https://processo-kappa.vercel.app",
+        "https://servidor-local-front-ghost.vercel.app",
+        "https://gulugulu-teal.vercel.app",
+        "https://servidor-local-front-ismar-dev.vercel.app",
+        "https://servidor-local-front-ismar.vercel.app",
+        "https://gulugulu-kappa.vercel.app",
+        "https://gulugulu-two.vercel.app",
+        "https://dev-05gulu.vercel.app",
+        "https://gulugulu-mocha.vercel.app",
+        "https://gulugulu-amber.vercel.app",
+        "https://dev-gulugulu-ismael.vercel.app",
+        "https://gulugulu-six.vercel.app",
+        "https://gulugulu-nu.vercel.app"
+    ],
     credentials: true,
     allowedHeaders: ["Content-Type", "authorization"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
