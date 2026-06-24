ada37ddc31aefae0945b950e565be388c6dd7372
Djeison Santos <djeison.adventista@gmail.com>
2026-06-24 09:36:45 -0100

    teste-carga.js

--- servidor-local/src/script/teste-carga.js
+++ servidor-local/src/script/teste-carga.js
@@ -7,7 +7,7 @@ export const options = {
 };
 
 export default function () {
-    const url = "https://servidor-local-center-backend-wt4z.onrender.com"; // URL do endpoint a ser testado
+    const url = "https://servidor-local-center-backend-wt4z.onrender.com/users/login"; // URL do endpoint a ser testado
 
      const payload = JSON.stringify({
         email: "a@gmail.com",
