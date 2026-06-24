aa5e578fc7f6bde9c64f76f3ec12fa7278b65b61
Djeison Santos <djeison.adventista@gmail.com>
2026-06-24 10:02:40 -0100

    teste-carga-login.js

--- servidor-local/src/script/teste-carga-login.js
+++ servidor-local/src/script/teste-carga-login.js
@@ -7,29 +7,30 @@ export const options = {
 };
 
 export default function () {
-    const url = "https://servidor-local-center-backend-wt4z.onrender.com/users/login"; // URL do endpoint a ser testado
+    const url =
+        "https://servidor-local-center-backend-wt4z.onrender.com/users/login"; // URL do endpoint a ser testado
 
-     const payload = JSON.stringify({
+    const payload = JSON.stringify({
         email: "a@gmail.com",
         password: "1234",
     });
 
-   
     const params = {
         headers: {
             "content-Type": "application/json",
             Origin: "https://again-liart.vercel.app/login", // funge que és teu frontend
-    "User-Agent": "k6 load test", // cabeçalho User-Agent para identificar o teste
+            "User-Agent": "k6 load test", // cabeçalho User-Agent para identificar o teste
         },
     };
 
     const response = http.post(url, payload, params);
     if (response.status !== 200) {
         console.log(
-            `Erro! Status: ${response.status} | Resposta do Servidor: ${response.body}`);
-    };
+            `Erro! Status: ${response.status} | Resposta do Servidor: ${response.body}`,
+        );
+    }
 
-    check(response, { 
+    check(response, {
         "Login Bem-sucedido": (r) => r.status === 200,
         "Login Rapido (Tempo < 500ms)": (r) => r.timings.duration < 500, // tempo de resposta menor que 500ms
         "CPU Esgotado (Erro 502/504)": (r) => r.status >= 500, // não deve retornar erro de CPU esgotado
