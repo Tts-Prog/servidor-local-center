4995541c8eac50484df5529d9f0737a1ce6ef24e
Hericles Lopes <hericlesmilton@gmail.com>
2026-06-24 12:28:11 -0100

    feat: add k6 load testing scripts for login and service retrieval endpoints

---
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -0,0 +1,56 @@
+import http from "k6/http"
+import {check, sleep} from "k6"
+
+export const options = {
+    vus: 20,
+    duration: "30s",
+}
+
+export  function  setup() {
+    const url = "https://servidor-local-center-backend-0yv2.onrender.com/users/login"
+
+    const payload = JSON.stringify({
+        email: "furria1@gmail.com",
+        password: "labanta2526",
+    });
+    
+    const params = {
+        headers:{
+            "Content-Type":"application/json",
+            "Origin":"https://servidor-local-center.vercel.app",
+            "User-Agent":"k6-load-test",
+        },
+    };
+    const res = http.post(url, payload, params);
+
+    return { token: res.json("token") }
+       
+}
+
+export default function(data){
+    const url ="https://servidor-local-center-backend-0yv2.onrender.com/services/get-all-servico-detalhado"
+
+    const params = {
+        headers:{
+           Autorization:`Bearer ${data.token}`,
+           "Content-Type":"application/json",
+           "User-Agent":"k6-load-test",
+        },
+
+        user:{
+            role:"ADMIN"
+        }
+    }
+    const res = http.get(url, params);
+
+    check(res, {
+        "Sucesso: ": (r) => r.status === 200,
+        "Rapido ( < 500ms): ": (r) => r.timings.duration < 500,
+        "Erro de servidor (Erro 502/504): ": (r) => r.status >= 500,
+        
+    })
+    sleep(1);
+    
+}
+
+
