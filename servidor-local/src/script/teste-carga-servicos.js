2ed080e33adc73948b4b0627e6334d3ebb7fe70e
Lúcio santos <lucio.baixada6@gmail.com>
2026-06-25 10:00:45 -0100

    feat: add express-status-monitor and morgan for improved logging and monitoring; create load testing script

--- servidor-local/src/script/teste-carga-servicos.js
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -2,54 +2,54 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-  vus: 50,
-  duration: "2m",
-};
+    vus: 50,
+    duration: "30s",
+}
 
 export function setup() {
-  // const loginUrl = "https://servidor-local-center-backend2.onrender.com/users/login";
-  const loginUrl = "http://api:8080/users/login";
+    const loginUrl =
+        "https://servidor-local-center-backend-kh04.onrender.com/users/login";
 
-  const payload = JSON.stringify({
-    email: "elvizoarez1@gmail.com",
-    password: "Webpass2334!",
-  });
 
-  const params = {
-    headers: {
-      "Content-Type": "application/json",
-      "User-Agent": "k6-load-test",
-      Origin: "https://servidor-local-center-three.vercel.app",
-    },
-  };
+    const payload = JSON.stringify({
+        email: "user@example.com",
+        password: "password123"
+    });
 
-  const res = http.post(loginUrl, payload, params);
+    const params = {
+        headers: {
+            "Content-Type": "application/json",
+            "User-Agent": "k6-load-test",
+            origin: "caveira-git-main-luciobaixada6-hashs-projects.vercel.app",
+        }
+    }
+    const res = http.post(loginUrl, payload, params);
 
-  return { token: res.json("token") };
+    return { token: res.json("token") }
 }
-
 export default function (data) {
-  const url = "http://api:8080/service/get-all-detailed";
+    const url = "https://servidor-local-center-backend-kh04.onrender.com/services/get-all-servico-detalhado/k6"; // URL do endpoint a ser testado
 
-  const params = {
-    headers: {
-      Authorization: `Bearer ${data.token}`,
-      "Content-Type": "application/json",
-      "User-Agent": "k6-load-test",
-    },
+    const params = {
+        headers: {
+            "Authorization": `Bearer ${data.token}`,
+            "Content-Type": "application/json",
+            "User-Agent": "k6-load-test",
 
-    user: {
-      role: "ADMIN",
-    },
-  };
+        },
 
-  const res = http.get(url, params);
+        user:{
+            role: "admin"
+        }
+    }
+    const res= http.get(url, params)
 
-  check(res, {
-    "Sucesso: ": (r) => r.status === 200,
-    "Rápido (< 500ms)": (r) => r.timings.duration < 500,
-    "Erro de servidor (Erro 502/504)": (r) => r.status >= 500,
-  });
+    check(res, {
+        "Sucesso:": (r) => r.status === 200,
+        "rapido (< 500ms)": (r) => r.timings.duration < 500,
+        "Erro de servidor (Erro 502/504)": (r) => r.status >= 500,
+    });
 
-  sleep(1);
+    sleep(1); 
 }
+
