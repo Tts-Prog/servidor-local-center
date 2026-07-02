f9ded23920271f633dde489e487cedd8d48d684e
Denis <deniskeys1023@gmail.com>
2026-07-02 10:51:35 -0100

    commit 1

---
+++ servidor-local/src/script/teste-carga-login.js
@@ -0,0 +1,44 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+    vus: 20, // número de usuários virtuais
+    duration: "2m", // duração do teste
+};
+
+export default function () {
+    //const url = "https://servidor-local-center-backend-0yv2.onrender.com/users/login"; // URL do endpoint a ser testado
+    const url = "https://api:8080/users/login"; // URL do endpoint a ser testado
+
+
+    const payload = JSON.stringify({
+        email: "furria1@gmail.com",
+        password: "labanta2526",
+    });
+
+    //const headers = {
+    //    "Content-type":"application/json"
+    //}
+
+    const params = {
+        headers:{
+            "Content-Type": "application/json",
+            "Origin": "https://servidor-local-center.vercel.app",
+            "User-Agent":"k6-load-test",
+        },
+    };
+
+    const response = http.post(url, payload, params);
+    if (response.status !== 200) {
+        console.log(
+            `ERRO! Status: ${response.status} | Resposta do Servidor: ${response.body}`,
+        );
+    }
+    check(response, { 
+        "Login Bem-sucedido": (r) => r.status === 200,
+        "Login Rapido (Tempo < 500ms)": (r) => r.timings.duration < 500, // tempo de resposta menor que 500ms
+        "CPU Esgotado (Erro 502/504)": (r) => r.status >= 500, // não deve retornar erro de CPU esgotado
+    });
+    
+    sleep(1); // espera 1 segundo entre as requisições
+}
---
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -0,0 +1,56 @@
+import http from "k6/http"
+import {check, sleep} from "k6"
+
+export const options = {
+    vus: 50,
+    duration: "2m",
+}
+
+//export  function  setup() {const url = "https://servidor-local-center-backend-0yv2.onrender.com/users/login"
+export  function  setup() {const url = "https://api:8080/users/login"
+
+
+
+    const payload = JSON.stringify({
+        email: "denis@gmail.com",
+        password: "labanta456",
+    });
+    
+    const params = {
+        headers:{
+            "Content-Type":"application/json",
+            "Origin":"https://servidor-local-center.vercel.app",
+            "User-Agent":"k6-load-test",
+        },
+    };
+    const res = http.post(url, payload, params);
+
+    return { token: res.json("token") }
+       
+}
+
+export default function(data){
+    const url ="https://servidor-local-center-backend-0yv2.onrender.com/services/get-all-servico-detalhado"
+
+    const params = {
+        headers:{
+           Autorization:`Bearer ${data.token}`,
+           "Content-Type":"application/json",
+           "User-Agent":"k6-load-test",
+        },
+
+        user:{
+            role:"ADMIN"
+        }
+    }
+    const res = http.get(url, params);
+
+    check(res, {
+        "Sucesso: ": (r) => r.status === 200,
+        "Rapido ( < 500ms): ": (r) => r.timings.duration < 500,
+        "Erro de servidor (Erro 502/504): ": (r) => r.status >= 500,
+        
+    })
+    sleep(1);
+    
+}
