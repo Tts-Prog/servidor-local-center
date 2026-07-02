23f689ee56df501c618a9fe652524ac19509b68d
jaquilinagomes <jakscv@gmail.com>
2026-07-02 11:36:49 -0100

    add change on file dg pg

--- servidor-local/src/script/teste-carga-login.js
+++ servidor-local/src/script/teste-carga-login.js
@@ -3,12 +3,13 @@ import { check, sleep } from "k6";
 
 export const options = {
     vus: 20, // número de usuários virtuais
-    duration: "30s", // duração do teste
+    duration: "2m", // duração do teste
 };
 
 export default function () {
-    const url =
-    "https://servidor-local-center-backend-ifql.onrender.com/users/login"; // URL do endpoint a ser testado
+    // const url =
+    // "https://servidor-local-center-backend-ifql.onrender.com/users/login"; // URL do endpoint a ser testado
+    const url = "http://api:8080/users/login"; // URL do endpoint a ser testado
 
     const payload = JSON.stringify({
     email: "bella@gmail.com",
--- servidor-local/src/script/teste-carga-servicos.js
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -3,12 +3,13 @@ import { check, sleep } from "k6";
 
 export const options = {
     vus: 50,
-    duration: "30s",
+    duration: "2m",
 }
 
 export function setup() {
-    const loginUrl = 
-    "https://servidor-local-center-backend-ifql.onrender.com/users/login";
+    // const loginUrl = 
+    // "https://servidor-local-center-backend-ifql.onrender.com/users/login";
+    const loginUrl = "http://api:8080/users/login";
 
     const payload = JSON.stringify({
     email: "elvis@gmail.com",
