0e4231c2875df13f790c8fabfd22cf73e527612a
Annette Carvalho <varelannette0@gmail.com>
2026-07-02 12:28:13 -0100

    exercicio 6 apagao

--- servidor-local/src/script/teste-carga-login.js
+++ servidor-local/src/script/teste-carga-login.js
@@ -11,8 +11,8 @@ export default function () {
   const url = "http://api:8080/users/login"; // URL do endpoint a ser testado
 
     const payload = JSON.stringify({
-        email: "z@gmail.com",
-        password: "9999",
+        email: "teste1@gmail.com",
+        password: "1234",
     });
 
   // const headers = {
