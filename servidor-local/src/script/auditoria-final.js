02e1641199c95edd98bdd6d4cb90321783b199d3
Kleber Veiga <Klebercorreia532@gmail.com>
2026-07-06 11:24:20 -0100

    feat: add auditing workflow and k6 script for performance testing

---
+++ servidor-local/src/script/auditoria-final.js
@@ -0,0 +1,51 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+    vus: 60,
+    duration: '1m',
+
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95)<600'],
+    },
+};
+
+export function setup() {
+
+    const login = http.post(
+        'http://api:8080/users/login',
+        JSON.stringify({
+            email: 'admin@email.com',
+            password: '123456'
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+
+    return {
+        token: login.json().token
+    };
+}
+
+export default function (data) {
+
+    const res = http.get(
+        'http://api:3000/api/servicos',
+        {
+            headers: {
+                Authorization: `Bearer ${data.token}`
+            }
+        }
+    );
+
+    check(res, {
+        'Status OK': (r) => r.status == 200 || r.status == 503
+    });
+
+
+    sleep(1);
+}
