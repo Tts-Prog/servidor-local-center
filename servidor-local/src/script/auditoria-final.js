9bb7eff53008520af16c89a0b3058e6abb96e80d
Helio Barbosa <barbosahifb1997@gmail.com>
2026-07-09 11:39:07 -0100

    exercicio k6

--- servidor-local/src/script/auditoria-final.js
+++ servidor-local/src/script/auditoria-final.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import {check, sleep} from "k6";
+import { check, sleep } from "k6";
 
 export const options = {
     vus: 60,
@@ -9,9 +9,10 @@ export const options = {
 export function setup() {
     //const loginUrl = "https://servidor-local-center-1rnu.onrender.com/users/login"
     const loginUrl = "http://api:8080/users/login"
+
     const payload = JSON.stringify({
-        email: "helioideino11@gmail.com",
-        password: "micro12345"
+        email: "1@gmail.com",
+        password: "123456789"
     });
 
     const params = {
@@ -24,30 +25,41 @@ export function setup() {
 
     const res = http.post(loginUrl, payload, params);
 
+    //console.log(`Login status: ${res.status} | Body: ${res.body}`);
 
-    return { token: res.json().data.token}
+    return { token: res.json().data.token }
 }
 
-export default function(data) {
-    const url = "http://api:8080/service/get-all-detailed"
+export default function (data) {
+    const url = "http://api:8080/service/create"
+
+    const payload = JSON.stringify({
+        nome: "Servico de Limpeza",
+        descricao: "Servico de Limpeza Profissional para residentes e empresa",
+        categoria: "Limpeza",
+        enabled_at: "true"
+    });
 
     const params = {
-        headrs: {
+
+        headers: {
             Authorization: `Bearer ${data.token}`,
             "Content-Type": "application/json",
             "User-Agent": "k6-load-test",
         },
         user: {
-            role:"ADMIN"
-        }
-    }
-    const res = http.get(url,params)
+            role: "ADMIN"
+        },
 
-    check(res,{
-        "Sucesso: ": (r) => r.status ===200,
+    }
+    const res = http.post(url, payload, params)
+
+    check(res, {
+        "Sucesso: ": (r) => r.status === 200,
         "Rapido (<500)": (r) => r.timings.duration < 500,
         "Erro de servidor (Erro 502/504)": (r) => r.status >= 500,
     })
 
     sleep(1)
+
 }
