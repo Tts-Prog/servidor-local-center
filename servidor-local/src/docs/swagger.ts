07cc5cf13be2838235c64cda070ed934decfa31a
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-07-09 09:16:14 -0100

    feat: implement RBAC authentication middleware and configure API routes with Swagger documentation

--- servidor-local/src/docs/swagger.ts
+++ servidor-local/src/docs/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJsdoc.Options = {
         },
         servers: [
             {
-                url: process.env.API_URL || `http://localhost:8080`,
+                url: process.env.API_URL || `http://localhost:8081`,
                 description: `dev`,
             }
         ],
