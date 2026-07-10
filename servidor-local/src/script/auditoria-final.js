acf06d2fadf5d57c8619d350b5060008c59a309a
Fábio Barros <brainsonfire85@gmail.com>
2026-07-10 10:45:51 -0100

    fix: update performance test duration to 1 minute and add GitHub Actions workflow for K6 testing

--- servidor-local/src/script/auditoria-final.js
+++ servidor-local/src/script/auditoria-final.js
@@ -3,7 +3,7 @@ import { check, sleep } from "k6";
 
 export const options = {
   vus: 60,
-  duration: "30s",
+  duration: "1m",
 };
 
 //export  function  setup() {const url = "https://servidor-local-center-backend-0yv2.onrender.com/users/login"
