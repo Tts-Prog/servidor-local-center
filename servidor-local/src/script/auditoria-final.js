5817a0d7cbe70d1a2e45828e125782f64e1f105f
Hericles Lopes <hericlesmilton@gmail.com>
2026-07-10 10:46:01 -0100

    feat: add Docker infrastructure, RBAC middleware, and K6 performance auditing workflows

--- servidor-local/src/script/auditoria-final.js
+++ servidor-local/src/script/auditoria-final.js
@@ -12,47 +12,55 @@ export  function  setup() {const url = "http://api:8080/users/login"
 
 
     const payload = JSON.stringify({
-        email: "furria@gmail.com",
+        email: "furria2@gmail.com",
         password: "furria1",
     });
     
     const params = {
         headers:{
             "Content-Type":"application/json",
-            "Origin":"https://servidor-local-center.vercel.app",
+            Origin:"https://servidor-local-center.vercel.app",
             "User-Agent":"k6-load-test",
         },
     };
     const res = http.post(url, payload, params);
-
-    return { token: res.json("token") }
-       
-}
-
-export default function(data){
-    const url ="https://servidor-local-center-backend-0yv2.onrender.com/services/get-all-servico-detalhado"
-
-    const params = {
-        headers:{
-           Autorization:`Bearer ${data.token}`,
-           "Content-Type":"application/json",
-           "User-Agent":"k6-load-test",
-        },
-
-        user:{
-            role:"ADMIN"
-        }
-    }
-    const res = http.get(url, params);
-
-    check(res, {
-        "Sucesso: ": (r) => r.status === 200,
-        "Rapido ( < 500ms): ": (r) => r.timings.duration < 500,
-        "Erro de servidor (Erro 502/504): ": (r) => r.status >= 500,
-        
-    })
-    sleep(1);
     
+    return { token: res.json().data.token };
 }
 
+
+export default function (data) {
+  const url = "http://api:8080/service/create"
+
+  const params = {
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+      "Content-Type": "application/json",
+      "User-Agent": "k6-load-test",
+    },
+    
+    
+
+    user: {
+      role: "ADMIN",
+    },
+  };
+   const payload = JSON.stringify({
+    nome: "Servico de Limpeza",
+    descricao: "Servico de Limpeza Profissional para residentes e empresa",
+    categoria: "Limpeza",
+    enabled_at: true
+  });
+
+  const res = http.post(url, payload,params);
+
+  check(res, {
+    "Sucesso: ": (r) => r.status === 200,
+    "Rápido (< 500ms)": (r) => r.timings.duration < 500,
+    "Erro de servidor (Erro 502/504)": (r) => r.status >= 500,
+  });
+
+
+  sleep(1);
+}
 
