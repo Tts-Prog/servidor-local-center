15969572f918319cd8bd35b9bc18576dc6d46311
cleiton de barros <cleitondebarros297@gmail.com>
2026-07-13 12:26:43 -0100

    fix: improve token handling in authorization header for load testing script

--- servidor-local/src/script/teste-carga-servicos.js
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -37,7 +37,7 @@ export default function (data) {
 
     const params = {
         headers: {
-            Authorization: `Bearer ${data.token}`,
+            Authorization: `Bearer ${data && data?.token ? data.token : ""}`,
             "Content-Type": "application/json",
             "User-Agent": "K6 load test",
         },
