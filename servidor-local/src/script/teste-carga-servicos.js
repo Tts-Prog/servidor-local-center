a96ad225073530e58e72137106dd9dca139babb8
Lúcio santos <lucio.baixada6@gmail.com>
2026-07-13 12:22:24 -0100

    atualization codigo

--- servidor-local/src/script/teste-carga-servicos.js
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -40,7 +40,7 @@ export default function (data) {
 
     const params = {
         headers: {
-            "Authorization": `Bearer ${data.token}`,
+            "Authorization": `Bearer ${data && data?.token? data?.token : ""}`,
             "Content-Type": "application/json",
             "User-Agent": "k6-load-test",
 
