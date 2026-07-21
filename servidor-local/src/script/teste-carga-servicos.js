52a76038c10f846f48a5646eae6ee84a2a30d210
h3ricl3s <hericlesmilton@gmail.com>
2026-07-21 12:41:27 +0000

    feat: update Docker Compose configuration and add Prometheus setup for performance testing

--- servidor-local/src/script/teste-carga-servicos.js
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -2,8 +2,11 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-  vus: 50,
-  duration: "2m",
+  stages: [
+    { duration: "30s", target: 50 },
+    { duration: "1m", target: 200 }, // 200 utilizadores em simultâneo!
+    { duration: "30s", target: 0 },
+  ],
 };
 
 export function setup() {
@@ -58,3 +61,8 @@ export default function (data) {
 
   sleep(1);
 }
+
+
+
+
+//docker compose up -d --force-recreate k6_attack && docker compose run --rm --entrypoint /bin/sh k6_attack -lc 'ls -l /app/src/script/teste-carga-servicos.js && ls -l /app'
