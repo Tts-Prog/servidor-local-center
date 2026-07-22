b3afd278ade94e687babdb3584f17ae420052c08
stiven <diasstiven2@gmail.com>
2026-07-22 09:16:58 -0100

    feat: add load testing environment with k6, Prometheus, and Grafana via Docker Compose

--- servidor-local/src/script/teste-carga-servicos.js
+++ servidor-local/src/script/teste-carga-servicos.js
@@ -14,8 +14,8 @@ export function setup() {
   const loginUrl = "http://api:8080/users/login";
 
    const payload = JSON.stringify({
-    email: "elvizoarez1@gmail.com",
-    password: "Webpass2334!",
+    email: "teste1gmail.com",
+    password: "123456789",
   });
 
   const params = {
